858706e794599e1f3e8ef0eefd4e05bd4b534520
e <thehumanej@gmail.com>
2024-01-25 13:53:40 -0600

    some css work

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+//import { Container } from "reactstrap";
 
 let alliance;
 let autoamp = 0;
@@ -28,43 +29,42 @@ function bluebtnfunc() {
   console.log(alliance);
 }
 
-
 function App() {
   return (
-    <div>
-      <h2 className="basicinfo-text">Team Info</h2>
-        <h3 className='tn-text'>Team Number</h3>
+      <div class='container'>
+        <h2 class="basicinfo-text">Team Info</h2>
+        <h3 class='tn-text'>Team Number</h3>
           <input type='text'></input>
-        <h3 className='alliance-text'>Alliance</h3>
+        <h3 class='alliance-text'>Alliance</h3>
           <button onClick={redbtnfunc} className='red-btn'>Red</button>
           <button onClick={bluebtnfunc} className='blue-btn'>Blue</button>
 
-      <h2 className='auto-points-text'>Auto Points</h2>
-        <h3 className='left-start'>Robot left start</h3>
-        <input className="left-start-cb" type="checkbox"/>
-        <h3 className='auto-amp-text'>Amp score</h3>
-          <button onClick={changeValue(autoamp,"add")} className='auto-amp-btn1'>+</button>
-          <button onClick={changeValue(autoamp,"subtract")} className='auto-amp-btn2'>-</button>
-        <h3 className='auto-speaker-text'>Speaker score</h3>
-          <button onClick={changeValue(autospeaker,"add")} className='auto-speaker-btn1'>+</button>
-          <button onClick={changeValue(autospeaker,"subtract")} className='auto-speaker-btn2'>-</button>
+      <h2 class='auto-points-text'>Auto Points</h2>
+        <h3 class='left-start'>Robot left start</h3>
+        <input class="left-start-cb" type="checkbox"/>
+        <h3 class='auto-amp-text'>Amp score</h3>
+          <button onClick={changeValue(autoamp,"add")} class='normal-button'>+</button>
+          <button onClick={changeValue(autoamp,"subtract")} class='normal-button'>-</button>
+        <h3 class='auto-speaker-text'>Speaker score</h3>
+          <button onClick={changeValue(autospeaker,"add")} class='normal-button'>+</button>
+          <button onClick={changeValue(autospeaker,"subtract")} class='normal-button'>-</button>
 
-      <h2 className='teleop-points-text'>Teleop Points</h2>
-        <h3 className='teleop-amp-text'>Amp score</h3>
-          <button onClick={changeValue(teleamp,"add")} className='teleop-amp-btn1'>+</button>
-          <button onClick={changeValue(teleamp,"subtract")} className='teleop-amp-btn2'>-</button>
-        <h3 className='teleop-speaker-text1'>Unamplified Speaker Score</h3>
-          <button onClick={changeValue(telespeaker_u,"add")} className='teleop-u-speaker-btn1'>+</button>
-          <button onClick={changeValue(telespeaker_u,"subtract")} className='teleop-u-speaker-btn2'>-</button>
-        <h3 className='teleop-speaker-text2'>Amplified Speaker Score</h3>
-          <button onClick={changeValue(telespeaker_a,"add")} className='teleop-a-speaker-btn1'>+</button>
-          <button onClick={changeValue(telespeaker_a,"subtract")} className='teleop-a-speaker-btn2'>-</button>
+      <h2 class='teleop-points-text'>Teleop Points</h2>
+        <h3 class='teleop-amp-text'>Amp score</h3>
+          <button onClick={changeValue(teleamp,"add")} class='teleop-amp-btn1'>+</button>
+          <button onClick={changeValue(teleamp,"subtract")} class='teleop-amp-btn2'>-</button>
+        <h3 class='teleop-speaker-text1'>Unamplified Speaker Score</h3>
+          <button onClick={changeValue(telespeaker_u,"add")} class='teleop-u-speaker-btn1'>+</button>
+          <button onClick={changeValue(telespeaker_u,"subtract")} class='teleop-u-speaker-btn2'>-</button>
+        <h3 class='teleop-speaker-text2'>Amplified Speaker Score</h3>
+          <button onClick={changeValue(telespeaker_a,"add")} class='teleop-a-speaker-btn1'>+</button>
+          <button onClick={changeValue(telespeaker_a,"subtract")} class='teleop-a-speaker-btn2'>-</button>
         <h3>Coopertition Point</h3>
-        <input className="coop-checkbox" type="checkbox"/>
+        <input class="checkbox" type="checkbox"/>
         
         <br></br>
         <br></br>
-        <input type='submit' name='submithjasdaks'/>
+        <input type='submit'/>
         <br></br>
         <br></br>
     </div>
