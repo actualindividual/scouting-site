730ad1062e01d6de531ffe4010da7bb47f0fc6e9
e <thehumanej@gmail.com>
2024-01-27 13:12:53 -0600

    it works now, text still not incrementing though :/

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
-import { useState } from 'react';
+//import { useState } from 'react';
 
 let alliance;
+let autoamp = 0;
+let autospeaker = 0;
+let teleamp = 0;
+let telespeaker_a = 0;
+let telespeaker_u = 0;
 
 function redbtnfunc() {
   alliance = "red";  
@@ -12,42 +17,57 @@ function bluebtnfunc() {
   console.log(alliance);
 }
 
+//naming button handlers like this is probably not good practice
+//but it works so...
+
+function button_handler1() {
+  autoamp = autoamp + 1;
+  console.log(autoamp);
+}
+function button_handler2() {
+  autoamp = autoamp - 1;
+  console.log(autoamp);
+}
+function button_handler3() {
+  autospeaker = autospeaker + 1;
+  console.log(autospeaker);
+}
+function button_handler4() {
+  autospeaker = autospeaker - 1;
+  console.log(autospeaker);
+}
+function button_handler5() {
+  teleamp = teleamp + 1;
+  console.log(teleamp);
+}
+function button_handler6() {
+  teleamp = teleamp - 1;
+  console.log(teleamp);
+}
+function button_handler7() {
+  telespeaker_u = telespeaker_u + 1;
+  console.log(telespeaker_u);
+}
+function button_handler8() {
+  telespeaker_u = telespeaker_u - 1;
+  console.log(telespeaker_u); 
+}
+function button_handler9() {
+  telespeaker_a = telespeaker_a + 1;
+  console.log(telespeaker_a);
+}
+function button_handler10() {
+  telespeaker_a = telespeaker_a - 1;
+  console.log(telespeaker_a);
+}
 
 function App() {
-  const [autoamp, setAutoAmp] = useState(0);
-  const [autospeaker, setAutoSpeaker] = useState(0);
-  const [teleamp, setTeleAmp] = useState(0);
-  const [telespeaker_u, setTeleSpeakerU] = useState(0);
-  const [telespeaker_a, setTeleSpeakerA] = useState(0);
+  //const [autoamp, setAutoAmp] = useState(0);
+  //const [autospeaker, setAutoSpeaker] = useState(0);
+  //const [teleamp, setTeleAmp] = useState(0);
+  //const [telespeaker_u, setTeleSpeakerU] = useState(0);
+  //const [telespeaker_a, setTeleSpeakerA] = useState(0);
 
-
-  function buttonHandlerPositive(e) {
-    if (e.target.id === 'autoamp+') {
-      setAutoAmp(autoamp + 1);
-    } else if (e.target.id === "autospeaker+") {
-      setAutoSpeaker(autospeaker + 1);
-    } else if (e.target.id === 'teleamp+') {
-      setTeleAmp(teleamp + 1);
-    } else if (e.target.id === 'telespeaker_u+') {
-      setTeleSpeakerU(telespeaker_u + 1);
-    } else if (e.target.id === 'telespeaker_a+') {
-      setTeleSpeakerA(telespeaker_a + 1);
-    }
-  }
-  
-  function buttonHandlerNegative(e) {
-    if (e.target.id === 'autoamp-') {
-      setAutoAmp(autoamp - 1);
-    } else if (e.target.id === "autospeaker-") {
-      setAutoSpeaker(autospeaker - 1);
-    } else if (e.target.id === 'teleamp-') {
-      setTeleAmp(teleamp - 1);
-    } else if (e.target.id === 'telespeaker_u-') {
-      setTeleSpeakerU(telespeaker_u - 1);
-    } else if (e.target.id === 'telespeaker_a-') {
-      setTeleSpeakerA(telespeaker_a - 1);
-    }
-  }
   return (
       <div class='container'>
         <h2 class="basicinfo-text">Basic Info</h2>
@@ -64,26 +84,26 @@ function App() {
         <input class="left-start-cb" type="checkbox"/>
         <h3 class='auto-amp-text'>Amp score</h3>
             <b id='autoamptext'>{autoamp}</b> <br></br><br></br>
-          <button onClick={e => buttonHandlerPositive} class='normal-button' id='autoamp+'>+</button>
-          <button onClick={e => buttonHandlerNegative} class='normal-button' id='autoamp-'>-</button>
+          <button onClick={button_handler1} class='normal-button' id='autoamp+'>+</button>
+          <button onClick={button_handler2} class='normal-button' id='autoamp-'>-</button>
         <h3 class='auto-speaker-text'>Speaker score</h3>
             <b id='autospeakertext'>{autospeaker}</b> <br></br><br></br>
-          <button onClick={e => buttonHandlerPositive} class='normal-button' id='autospeaker+'>+</button>
-          <button onClick={e => buttonHandlerNegative} class='normal-button' id='autospeaker-'>-</button>
+          <button onClick={button_handler3} class='normal-button' id='autospeaker+'>+</button>
+          <button onClick={button_handler4} class='normal-button' id='autospeaker-'>-</button>
 
       <h2 class='teleop-points-text'>Teleop Points</h2>
         <h3 class='teleop-amp-text'>Amp score</h3>
             <b id='teleamptext'>{teleamp}</b> <br></br><br></br>
-          <button onClick={e => buttonHandlerPositive} class='normal-button' id='teleamp+'>+</button>
-          <button onClick={e => buttonHandlerNegative} class='normal-button' id='teleamp-'>-</button>
+          <button onClick={button_handler5} class='normal-button' id='teleamp+'>+</button>
+          <button onClick={button_handler6} class='normal-button' id='teleamp-'>-</button>
         <h3 class='teleop-speaker-text1'>Unamplified Speaker Score</h3>
             <b id='teleamptext1'>{telespeaker_u}</b> <br></br><br></br>
-          <button onClick={e => buttonHandlerPositive} class='normal-button' id='telespeaker_u+'>+</button>
-          <button onClick={e => buttonHandlerNegative} class='normal-button' id='telespeaker_u-'>-</button>
+          <button onClick={button_handler7} class='normal-button' id='telespeaker_u+'>+</button>
+          <button onClick={button_handler8} class='normal-button' id='telespeaker_u-'>-</button>
         <h3 class='teleop-speaker-text2'>Amplified Speaker Score</h3>
             <b id='teleamptext2'>{telespeaker_a}</b> <br></br><br></br>
-          <button onClick={e => buttonHandlerPositive} class='normal_button' id='telespeaker_a+'>+</button>
-          <button onClick={e => buttonHandlerNegative} class='normal_button' id='telespeaker_a-'>-</button>
+          <button onClick={button_handler9} class='normal_button' id='telespeaker_a+'>+</button>
+          <button onClick={button_handler10} class='normal_button' id='telespeaker_a-'>-</button>
         <h3>Coopertition Point</h3>
         <input class="checkbox" type="checkbox"/>
         <br></br>
