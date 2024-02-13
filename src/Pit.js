a65b8843c6d39e8a994f4345dc5733cd520c6562
e <thehumanej@gmail.com>
2024-02-13 07:45:55 -0600

    migrated all stand functions to stand.js

---
+++ src/Pit.js
@@ -0,0 +1,20 @@
+import './Pit.css';
+import { useState } from 'react';
+
+
+
+export function uploadPit() {
+    //teamNumber: Int,
+	//scoutName: String,
+	//driveType: String,
+	//intake: String,
+	//playstyle: String,
+	//bestAuto: String,
+	//defense: Bool,
+	//speaker: Bool,
+	//climb: Bool,
+	//harmony: Bool,
+	//underStage: Bool,
+	//trap: Bool,
+	//extraNotes: String
+}
